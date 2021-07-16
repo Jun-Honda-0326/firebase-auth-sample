403d566030968e2f02434a4ff2b17e1054cc26ae
Jun-Honda <j.honda.0326@gmail.com>
2021-07-16 18:50:40 +0900

    fix indent

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,12 +4,12 @@
 
 export const environment = {
   production: false,
-	firebase: {
-		apiKey: "AIzaSyBJWnPJX9NSWkbDVQoUCBZfkDfb_jbJ-1Y",
-		authDomain: "angular-auth-sample-11261.firebaseapp.com",
-		projectId: "angular-auth-sample-11261",
-		storageBucket: "angular-auth-sample-11261.appspot.com",
-		messagingSenderId: "645159483965",
-		appId: "1:645159483965:web:d44ab1d7ea4d764a5a0854"
-	}
+  firebase: {
+    apiKey: "AIzaSyBJWnPJX9NSWkbDVQoUCBZfkDfb_jbJ-1Y",
+    authDomain: "angular-auth-sample-11261.firebaseapp.com",
+    projectId: "angular-auth-sample-11261",
+    storageBucket: "angular-auth-sample-11261.appspot.com",
+    messagingSenderId: "645159483965",
+    appId: "1:645159483965:web:d44ab1d7ea4d764a5a0854"
+  }
 };
